fab5d846603278f4d901747364c8b065a13d69c4
Safahadidy <safahadidy@gmail.com>
2023-08-28 04:30:18 +0300

    c

--- monty.c
+++ monty.c
@@ -9,31 +9,31 @@
  */
 int main(int argc, char **argv)
 {
-        FILE *file;
-        char *line = NULL;
-        size_t line_length = 0;
-        ssize_t read;
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
-        file = fopen(file_name, "r");
-        if (file == NULL)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", file_name);
-                exit(EXIT_FAILURE);
-        }
-        data.file = file;
-        while ((read = getline(&line, &line_length, file)) != -1)
-        {
-                data.line_number++;
-                data.line = line;
-                if (is_empty_line(data.line) == 1)
-			execute_line(my_data.line, my_data.line_number);
-	}
-	free_stack(my_data.stack);
-	fclose(file);
-	free(line);
-	return (0);
+FILE *file;
+char *line = NULL;
+size_t line_length = 0;
+ssize_t read;
+if (argc != 2)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+}
+file = fopen(file_name, "r");
+if (file == NULL)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", file_name);
+	exit(EXIT_FAILURE);
+}
+data.file = file;
+while ((read = getline(&line, &line_length, file)) != -1)
+{
+	data.line_number++;
+	data.line = line;
+	if (is_empty_line(data.line) == 1)
+		execute_line(my_data.line, my_data.line_number);
+}
+free_stack(my_data.stack);
+fclose(file);
+free(line);
+return (0);
 }
